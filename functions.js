ae1247dd91ffcfeeb0727388eb8d82e0cd8164eb
mahmud009 <mahmud.est@gmail.com>
2020-07-06 23:16:05 +0600

    Route A image fixed

--- functions.js
+++ functions.js
@@ -7,12 +7,12 @@ function init() {
   // based on dropdown selection
   let imgId = $("#pedestal-config-A").val();
   $("#img-section-2A img").css("opacity", "0");
-  $(`#${imgId}`).css("opacity", "1");
+  $(`#corners-${imgId}`).css("opacity", "1");
 
   $("#pedestal-config-A").on("change", function () {
     let imgId = $(this).val();
     $("#img-section-2A img").css("opacity", "0");
-    $(`#${imgId}`).css("opacity", "1");
+    $(`#corners-${imgId}`).css("opacity", "1");
   });
 }
 //xxxxxxxxxxxxxxx-- End of function --xxxxxxxxxxxxxxxxxxxxx
